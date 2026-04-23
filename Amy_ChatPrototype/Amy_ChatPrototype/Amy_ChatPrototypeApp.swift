a82fcc9b54361a657fbfa32078e35b199ea97bd6
Amy <chong26@student.kis.or.kr>
2026-04-23 13:03:58 +0900

    Create Chat Prototype Project

---
+++ Amy_ChatPrototype/Amy_ChatPrototype/Amy_ChatPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Amy_ChatPrototypeApp.swift
+//  Amy_ChatPrototype
+//
+//  Created by amy hong on 4/23/26.
+//
+
+import SwiftUI
+
+@main
+struct Amy_ChatPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
